bb1405bae5470bf4366dd122e69398c3f0062efa
Yoshimi Nukata <yoshimi.theconcourse@gmail.com>
2019-02-20 01:21:39 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 
   root to: 'tasks#index'
